c3149569b1df98a41c93925df2ba8c5295405607
lesya89 <zolota98@mail.ru>
2019-04-27 23:22:48 +0300

    add second test

--- modules/print-simple-numbers/test/test_print_simple_numbers.cpp
+++ modules/print-simple-numbers/test/test_print_simple_numbers.cpp
@@ -4,13 +4,25 @@
 
 #include "include/print-simple-numbers.h"
 
-TEST(SimpleNumberTet, InNullSizeArray) {
+TEST(SimpleNumberTest, The_Left_Border_Mistake) {
 // arrange
 SimpleNumbers check_eng;
 int a = -1;
 int b = 5;
 // act
-res = check_eng(a, b);
+res = check_eng.print(a, b);
 // assert
 EXPECT_EQ(-1, res);
 }
+
+TEST(SimpleNumbersTest, The_Right_Border_Mistake) {
+// arrange
+SimpleNumbers check_eng;
+int a = 6;
+int b = -7;
+// act
+res = check_eng.print(a, b);
+// assert
+EXPECT_EQ(-1, res);
+}
+   
