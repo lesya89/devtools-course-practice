1f46ca12700714af9f92b7c55520c0382d08473b
lesya89 <zolota98@mail.ru>
2019-04-29 01:24:46 +0300

    fix test 4

--- modules/print-simple-numbers/test/test_print_simple_numbers.cpp
+++ modules/print-simple-numbers/test/test_print_simple_numbers.cpp
@@ -4,18 +4,6 @@
 
 #include "include/print-simple-numbers.h"
 
-TEST(SimpleNumberTest, The_Left_Border_Mistake) {
-    // arrange
-    SimpleNumbers check_eng;
-    int a = -1;
-    int b = 5;
-    int res;
-    // act
-    res = check_eng.SizeOfArray(a, b);
-    // assert
-    EXPECT_EQ(-1, res);
-}
-
 TEST(SimpleNumbersTest, The_Right_Border_Mistake) {
     // arrange
     SimpleNumbers check_eng;
